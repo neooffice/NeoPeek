5e982bb7512c8f55d1c5e508d349e24559e2e255
peterlin <>
2007-06-02 19:19:33 +0000

    Change license to GPL.
    Edward Peterlin
    OPENSTEP@neooffice.org

--- common.mm
+++ common.mm
@@ -12,16 +12,16 @@
  *  The Contents of this file are made available subject to the terms of
  *  either of the following licenses
  *
- *         - GNU Lesser General Public License Version 2.1
+ *         - GNU General Public License Version 2.1
  *
  *  Edward Peterlin, 2005
  *
- *  GNU Lesser General Public License Version 2.1
+ *  GNU General Public License Version 2.1
  *  =============================================
  *  Copyright 2005 by Edward Peterlin (OPENSTEP@neooffice.org)
  *
  *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
+ *  modify it under the terms of the GNU General Public
  *  License version 2.1, as published by the Free Software Foundation.
  *
  *  This library is distributed in the hope that it will be useful,
@@ -29,7 +29,7 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *  General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public
+ *  You should have received a copy of the GNU General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  *  MA  02111-1307  USA
